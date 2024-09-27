06dac4831828a984d37e477b8d5da350cf47838f
Alan Lu <>
2024-09-27 17:45:36 +0800

    fix error

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,7 @@
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  time.timeZone = "Aisa/Shanghai";
+  time.timeZone = "Asia/Shanghai";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -91,6 +91,7 @@
     git
     tree
     neofetch
+    fzf
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
